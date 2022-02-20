68449462552c3193da13e52b41ac25614a09c42d
Owner <sozgur@asu.edu>
2022-02-20 13:21:59 -0700

    Created Reset Function in BaseQTable.py

--- Algorithms/BaseQTable.py
+++ Algorithms/BaseQTable.py
@@ -1,4 +1,8 @@
 class BaseQTable(object):
     def __init__(self, A_space, S_space):
-        self.ActionSpac = A_space
+        self.ActionSpace = A_space
         self.StateSpace = S_space
+        self.Q_table = []
+
+    def reset(self):
+        self.Q_table = []
